75b8840d17624c44e873d2b5709f5e3497126f54
artemiy <artklyachin@gmail.com>
2023-11-06 19:52:11 +0000

    first

--- task3/type_tuples.hpp
+++ task3/type_tuples.hpp
@@ -10,4 +10,16 @@ struct TTuple {};
 template<class TT>
 concept TypeTuple = requires(TT t) { []<class... Ts>(TTuple<Ts...>){}(t); };
 
+template<TypeTuple TT, typename T>
+using Prepend =
+decltype([]<class... Ts>(TTuple<Ts...>) {
+    return TTuple<T, Ts...>{};
+} (TT{}));
+
+template<TypeTuple TT, typename T>
+using Append =
+decltype([]<class... Ts>(TTuple<Ts...>) {
+    return TTuple<Ts..., T>{};
+} (TT{}));
+
 } // namespace type_tuples
